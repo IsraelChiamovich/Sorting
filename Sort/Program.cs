2f30264ef39ed5441a2a083e93ca4d33a8fbdefe
Israel Chiamovich <israel385385@gmail.com>
2024-09-01 10:34:33 +0300

    first commit

---
+++ Sort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sortArray = BubbleSort(new int[] {7, 4, 9, 3, 2, 1, 6, 8, 5, 10 });
+
+            Console.WriteLine(string.Join(", ", sortArray));
+        }
+
+        public static int[] BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
